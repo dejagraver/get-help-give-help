32cb5a1b76d081f8076228d2d98578f2798b2342
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-30 11:12:22 -0400

    adding more post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -10,4 +10,50 @@ router.get('/', (req,res) => {
     })
 });
 
+router.get('/:id', (req,res) => {
+    Post.findOne(
+        {
+            where: {
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'first_name', 'last_name', 'email']
+                },
+                {
+                    model: Category,
+                    attributes: ['id', 'name']
+                }
+            ]
+        }
+    )
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({message: 'No post found with that id.'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+// may need to accomodate for session
+router.post('/', (req,res) => {
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.body.user_id,
+        category_id: req.body.category_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
 module.exports = router;
