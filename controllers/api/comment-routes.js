a39041bb7107edac69710d64a2f37bfc4f0deae2
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-31 12:53:48 -0400

    adding comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
 const { User, Post, Category, Comment, UserCategory } = require('../../models');
 
+router.get('/', (req, res) => {
+    Comment.findAll({})
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// add session handling for post and delete routes
+
 module.exports = router;
