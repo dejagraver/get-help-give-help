7cc43e8bbb722e6be81a6361ec4203fc1a9b92cd
dejagraver <dejaamaartin@gmail.com>
2021-06-01 02:38:08 -0400

    added fire base npm, st up sign up, login, log out

---
+++ javascript/logout.js
@@ -0,0 +1,26 @@
+///this needs to be pasted into the body tag with in the HTML files 
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/8.6.3/firebase-app.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/8.6.3/firebase-login.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/8.6.3/firebase-analytics.js"></script>
+
+{/* <script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyD4a1zBHTz5WCT__Y7IaGvMFKlS03nknJI",
+    authDomain: "get-help-give-help-3fc47.firebaseapp.com",
+    projectId: "get-help-give-help-3fc47",
+    storageBucket: "get-help-give-help-3fc47.appspot.com",
+    messagingSenderId: "83016709803",
+    appId: "1:83016709803:web:8d79f75beb5c5f006769ee",
+    measurementId: "G-27VGBMGRRV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script> */}
