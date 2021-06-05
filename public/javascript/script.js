d91a25b5fb4f3b94c3b0cd03751bb395fb1071fe
dejagraver <dejaamaartin@gmail.com>
2021-06-05 09:42:28 -0400

    authentication session

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -48,7 +48,5 @@ async function loginFormHandler(event) {
 }
 
 
-
-
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
