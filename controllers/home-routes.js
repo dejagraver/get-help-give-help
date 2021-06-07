88cc4b7393923138c17008add5ad99bb4bf594c7
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-06-06 21:18:45 -0400

    added js for edit posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     res.locals.homePageStyleSheet = `style`;
@@ -14,10 +15,42 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('/editpost', (req, res) => {
+router.get('/edit/:id', (req, res) => {
     if (req.session.loggedIn) {
         // add stylesheet for editpost here
-        res.render('editpost', { loggedIn: req.session.loggedIn });
+        Post.findOne(
+            {
+                where: {
+                    id: req.params.id
+                },
+                include: [
+                    {
+                        model: Comment,
+                        attributes: ['id', 'comment_text', 'user_id', 'post_id'],
+                        include: {
+                            model: User,
+                            attributes: ['first_name', 'last_name', 'email']
+                        }
+                    },
+                    {
+                        model: User,
+                        attributes: ['first_name', 'last_name', 'email']
+                    }
+                ]
+            }
+        )
+            .then(dbPostData => {
+                if (!dbPostData) {
+                    res.status(404).end();
+                }
+                const post = dbPostData.get({ plain: true });
+
+                res.render('editpost', { post, loggedIn: true })
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
+        // res.render('editpost', { loggedIn: req.session.loggedIn });
     }
 });
 
